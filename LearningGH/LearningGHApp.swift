6ad81ec17cf93a86ba937112012e851947c626e1
manish <manishreddy30@icloud.com>
2024-04-25 18:12:34 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Manishreddy Gaddam on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
