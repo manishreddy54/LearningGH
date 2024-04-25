2a6df161bfd5b81929bed5c595d1bef5c64859ef
manish <manishreddy30@icloud.com>
2024-04-25 18:28:38 -0500

    Image modified

--- LearningGH/ContentView.swift
+++ LearningGH/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "figure.wave")
+                .resizable()
+                .frame(width: 100, height: 150)
             Text("what's up, CodeCrew!")
                 .padding()
         }
